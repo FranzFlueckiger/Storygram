242411bbacd8880db2ee801e27adfef0dd97eb42
Franz Flückiger <32476856+FranzFlueckiger@users.noreply.github.com>
2019-11-01 15:06:22 +0100

    complete preprocessing tests

--- test/PreProcessing.test.ts
+++ test/PreProcessing.test.ts
@@ -1,6 +1,6 @@
 import { KnotDiagram } from '../src/KnotDiagram'
 import { Config, } from '../src/Types'
-import { testArrayData, testTableData } from './testData'
+import { testArrayData, testTableData, testRangeData } from './testData'
 
 test('from array', () => {
   let config: Config = {
@@ -10,14 +10,11 @@ test('from array', () => {
   }
   let KD = new KnotDiagram(testArrayData(), config)
   expect(KD.data.xData.length).toEqual(10);
-  const layer4 = {
-    "add": [], "data": { "a": ["zf", "lf", "bf"], "id": 4 }, "group": ["zf", "lf", "bf"], "hiddenYs": [], "id": 4,
-    "isHidden": false, "remove": [], "state": [], "switch": [], "xValue": 4
-  }
-  expect(KD.data.xData[4]).toEqual(layer4);
-  const yFF = { "data": {}, "isHidden": false, "layers": [{ "add": [], "data": { "a": ["ff", "ef", "af", "zf"], "id": 1 }, "group": ["ff", "ef", "af", "zf"], "hiddenYs": [], "id": 1, "isHidden": false, "remove": [], "state": [], "switch": [], "xValue": 1 }, { "add": [], "data": { "a": ["ff", "gf"], "id": 2 }, "group": ["ff", "gf"], "hiddenYs": [], "id": 2, "isHidden": false, "remove": [], "state": [], "switch": [], "xValue": 2 }, { "add": [], "data": { "a": ["ff", "ef", "cf", "pf"], "id": 3 }, "group": ["ff", "ef", "cf", "pf"], "hiddenYs": [], "id": 3, "isHidden": false, "remove": [], "state": [], "switch": [], "xValue": 3 }, { "add": [], "data": { "a": ["pf", "ff"], "id": 7 }, "group": ["pf", "ff"], "hiddenYs": [], "id": 7, "isHidden": false, "remove": [], "state": [], "switch": [], "xValue": 7 }, { "add": [], "data": { "a": ["ff", "gf", "cf", "af"], "id": 8 }, "group": ["ff", "gf", "cf", "af"], "hiddenYs": [], "id": 8, "isHidden": false, "remove": [], "state": [], "switch": [], "xValue": 8 }], "yID": "ff" }
+  const layer4 = "{\"xValue\":4,\"data\":{\"a\":[\"zf\",\"lf\",\"bf\"],\"id\":4},\"switch\":[],\"isHidden\":false,\"add\":[],\"remove\":[],\"group\":[\"zf\",\"lf\",\"bf\"],\"state\":[],\"hiddenYs\":[],\"id\":4}"
+  expect(JSON.stringify(KD.data.xData[4])).toEqual(layer4);
+  const yFF = "{\"yID\":\"ff\",\"data\":{},\"isHidden\":false,\"layers\":[{\"xValue\":1,\"data\":{\"a\":[\"ff\",\"ef\",\"af\",\"zf\"],\"id\":1},\"switch\":[],\"isHidden\":false,\"add\":[],\"remove\":[],\"group\":[\"ff\",\"ef\",\"af\",\"zf\"],\"state\":[],\"hiddenYs\":[],\"id\":1},{\"xValue\":2,\"data\":{\"a\":[\"ff\",\"gf\"],\"id\":2},\"switch\":[],\"isHidden\":false,\"add\":[],\"remove\":[],\"group\":[\"ff\",\"gf\"],\"state\":[],\"hiddenYs\":[],\"id\":2},{\"xValue\":3,\"data\":{\"a\":[\"ff\",\"ef\",\"cf\",\"pf\"],\"id\":3},\"switch\":[],\"isHidden\":false,\"add\":[],\"remove\":[],\"group\":[\"ff\",\"ef\",\"cf\",\"pf\"],\"state\":[],\"hiddenYs\":[],\"id\":3},{\"xValue\":7,\"data\":{\"a\":[\"pf\",\"ff\"],\"id\":7},\"switch\":[],\"isHidden\":false,\"add\":[],\"remove\":[],\"group\":[\"pf\",\"ff\"],\"state\":[],\"hiddenYs\":[],\"id\":7},{\"xValue\":8,\"data\":{\"a\":[\"ff\",\"gf\",\"cf\",\"af\"],\"id\":8},\"switch\":[],\"isHidden\":false,\"add\":[],\"remove\":[],\"group\":[\"ff\",\"gf\",\"cf\",\"af\"],\"state\":[],\"hiddenYs\":[],\"id\":8}]}"
   expect(KD.data.yData.size).toEqual(10);
-  expect(KD.data.yData.get('ff')).toEqual(yFF);
+  expect(JSON.stringify(KD.data.yData.get('ff'))).toEqual(yFF);
 })
 
 test('from table', () => {
@@ -28,20 +25,25 @@ test('from table', () => {
   }
   const KD = new KnotDiagram(testTableData(), config)
   expect(KD.data.xData.length).toEqual(10);
-  const layer4 = {
-    xValue: 4,
-    data: { id: 4, a: 'zf', b: 'lf', c: 'bf' },
-    switch: [],
-    isHidden: false,
-    add: [],
-    remove: [],
-    group: ['zf', 'lf', 'bf'],
-    state: [],
-    hiddenYs: [],
-    id: 4
-  }
-  expect(KD.data.xData[4]).toEqual(layer4);
-  const yFF = "{\"yID\":\"ff\",\"data\":{},\"isHidden\":false,\"layers\":[{\"xValue\":1,\"data\":{\"id\":1,\"a\":\"ff\",\"b\":\"ef\",\"c\":\"af\",\"d\":\"zf\"},\"switch\":[],\"isHidden\":false,\"add\":[],\"remove\":[],\"group\":[\"ff\",\"ef\",\"af\",\"zf\"],\"state\":[],\"hiddenYs\":[],\"id\":1},{\"xValue\":3,\"data\":{\"id\":3,\"a\":\"ff\",\"b\":\"gf\"},\"switch\":[],\"isHidden\":false,\"add\":[],\"remove\":[],\"group\":[\"ff\",\"gf\"],\"state\":[],\"hiddenYs\":[],\"id\":2},{\"xValue\":3,\"data\":{\"id\":3,\"a\":\"ff\",\"b\":\"ef\",\"c\":\"cf\",\"d\":\"pf\"},\"switch\":[],\"isHidden\":false,\"add\":[],\"remove\":[],\"group\":[\"ff\",\"ef\",\"cf\",\"pf\"],\"state\":[],\"hiddenYs\":[],\"id\":3},{\"xValue\":7,\"data\":{\"id\":7,\"a\":\"pf\",\"b\":\"ff\"},\"switch\":[],\"isHidden\":false,\"add\":[],\"remove\":[],\"group\":[\"pf\",\"ff\"],\"state\":[],\"hiddenYs\":[],\"id\":7},{\"xValue\":8,\"data\":{\"id\":8,\"a\":\"ff\",\"b\":\"gf\",\"c\":\"cf\",\"d\":\"af\"},\"switch\":[],\"isHidden\":false,\"add\":[],\"remove\":[],\"group\":[\"ff\",\"gf\",\"cf\",\"af\"],\"state\":[],\"hiddenYs\":[],\"id\":8}]}"
+  const layer4 = "{\"xValue\":4,\"data\":{\"id\":4,\"a\":\"zf\",\"b\":\"lf\",\"c\":\"bf\"},\"switch\":[],\"isHidden\":false,\"add\":[],\"remove\":[],\"group\":[\"zf\",\"lf\",\"bf\"],\"state\":[],\"hiddenYs\":[],\"id\":4}"
+  expect(JSON.stringify(KD.data.xData[4])).toEqual(layer4);
+  const yFF = "{\"yID\":\"ff\",\"data\":{},\"isHidden\":false,\"layers\":[{\"xValue\":1,\"data\":{\"id\":1,\"a\":\"ff\",\"b\":\"ef\",\"c\":\"af\",\"d\":\"zf\"},\"switch\":[],\"isHidden\":false,\"add\":[],\"remove\":[],\"group\":[\"ff\",\"ef\",\"af\",\"zf\"],\"state\":[],\"hiddenYs\":[],\"id\":1},{\"xValue\":2,\"data\":{\"id\":2,\"a\":\"ff\",\"b\":\"gf\"},\"switch\":[],\"isHidden\":false,\"add\":[],\"remove\":[],\"group\":[\"ff\",\"gf\"],\"state\":[],\"hiddenYs\":[],\"id\":2},{\"xValue\":3,\"data\":{\"id\":3,\"a\":\"ff\",\"b\":\"ef\",\"c\":\"cf\",\"d\":\"pf\"},\"switch\":[],\"isHidden\":false,\"add\":[],\"remove\":[],\"group\":[\"ff\",\"ef\",\"cf\",\"pf\"],\"state\":[],\"hiddenYs\":[],\"id\":3},{\"xValue\":7,\"data\":{\"id\":7,\"a\":\"pf\",\"b\":\"ff\"},\"switch\":[],\"isHidden\":false,\"add\":[],\"remove\":[],\"group\":[\"pf\",\"ff\"],\"state\":[],\"hiddenYs\":[],\"id\":7},{\"xValue\":8,\"data\":{\"id\":8,\"a\":\"ff\",\"b\":\"gf\",\"c\":\"cf\",\"d\":\"af\"},\"switch\":[],\"isHidden\":false,\"add\":[],\"remove\":[],\"group\":[\"ff\",\"gf\",\"cf\",\"af\"],\"state\":[],\"hiddenYs\":[],\"id\":8}]}"
   expect(KD.data.yData.size).toEqual(10);
   expect(JSON.stringify(KD.data.yData.get('ff'))).toEqual(yFF);
+})
+
+test('from ranges', () => {
+  let config: Config = {
+    dataFormat: 'ranges',
+    yField: 'id',
+    startField: 'from',
+    endField: 'to'
+  }
+  const KD = new KnotDiagram(testRangeData(), config)
+  expect(KD.data.xData.length).toEqual(10);
+  const layer4 = "{\"xValue\":\"4\",\"data\":{},\"switch\":[],\"isHidden\":false,\"add\":[],\"remove\":[],\"group\":[\"1\",\"2\",\"3\",\"4\",\"6\"],\"state\":[],\"hiddenYs\":[]}"
+  expect(JSON.stringify(KD.data.xData[4])).toEqual(layer4);
+  const yFF = "{\"yID\":\"4\",\"data\":{\"id\":\"4\",\"from\":\"1\",\"to\":\"5\",\"c\":\"bf\"},\"isHidden\":false,\"layers\":[{\"xValue\":\"1\",\"data\":{},\"switch\":[],\"isHidden\":false,\"add\":[],\"remove\":[],\"group\":[\"0\",\"1\",\"4\",\"9\"],\"state\":[],\"hiddenYs\":[]},{\"xValue\":\"2\",\"data\":{},\"switch\":[],\"isHidden\":false,\"add\":[],\"remove\":[],\"group\":[\"0\",\"1\",\"2\",\"4\",\"9\"],\"state\":[],\"hiddenYs\":[]},{\"xValue\":\"3\",\"data\":{},\"switch\":[],\"isHidden\":false,\"add\":[],\"remove\":[],\"group\":[\"0\",\"1\",\"2\",\"4\",\"6\",\"9\"],\"state\":[],\"hiddenYs\":[]},{\"xValue\":\"4\",\"data\":{},\"switch\":[],\"isHidden\":false,\"add\":[],\"remove\":[],\"group\":[\"1\",\"2\",\"3\",\"4\",\"6\"],\"state\":[],\"hiddenYs\":[]},{\"xValue\":\"5\",\"data\":{},\"switch\":[],\"isHidden\":false,\"add\":[],\"remove\":[],\"group\":[\"1\",\"3\",\"4\",\"6\"],\"state\":[],\"hiddenYs\":[]}]}"
+  expect(KD.data.yData.size).toEqual(10);
+  expect(JSON.stringify(KD.data.yData.get('4'))).toEqual(yFF);
 })
--- test/testData.ts
+++ test/testData.ts
@@ -19,7 +19,7 @@ export function testTableData() {
     return [
         { id: 0, a: "bf", b: "gf", c: "kf" },
         { id: 1, a: "ff", b: "ef", c: "af", d: "zf" },
-        { id: 3, a: "ff", b: "gf" },
+        { id: 2, a: "ff", b: "gf" },
         { id: 3, a: "ff", b: "ef", c: "cf", d: "pf" },
         { id: 4, a: "zf", b: "lf", c: "bf" },
         { id: 5, a: "gf", b: "ef", c: "af", d: "pf" },
@@ -27,5 +27,20 @@ export function testTableData() {
         { id: 7, a: "pf", b: "ff" },
         { id: 8, a: "ff", b: "gf", c: "cf", d: "af" },
         { id: 9, a: "ef", b: "gf", c: "zf" },
+    ];
+}
+
+export function testRangeData() {
+    return [
+        { id: '0', from: "0", to: "3", c: "kf" },
+        { id: '1', from: "1", to: "6", c: "af", d: "zf" },
+        { id: '2', from: "2", to: "4" },
+        { id: '3', from: "4", to: "5", c: "cf", d: "pf" },
+        { id: '4', from: "1", to: "5", c: "bf" },
+        { id: '5', from: "8", to: "9", c: "af", d: "pf" },
+        { id: '6', from: "3", to: "8", c: "kf" },
+        { id: '7', from: "7", to: "8" },
+        { id: '8', from: "9", to: "9", c: "cf", d: "af" },
+        { id: '9', from: "1", to: "3", c: "zf" },
     ];
 }
