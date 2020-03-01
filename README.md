e4d882035ff6be0ec1ba4dbbb92ada385640ed6b
Franz Flückiger <32476856+FranzFlueckiger@users.noreply.github.com>
2020-03-01 13:28:30 +0100

    upd readme & docz

--- README.md
+++ README.md
@@ -1,9 +1,7 @@
 # The Storygram
 
 <p align="center">
-  <a href="https://storygram.netlify.com/" target="_blank">
     <img alt="Parcel" src="https://raw.githubusercontent.com/FranzFlueckiger/Storygram/master/doc/src/media/storyGram_Blockbuster.png" width="749">
-  </a>
 </p>
 
 The Storygram allows you to explore and visualize sequential groupings in your data.
--- doc/.docz/gatsby-config.js
+++ doc/.docz/gatsby-config.js
@@ -19,8 +19,8 @@ const config = {
   pathPrefix: '/',
 
   siteMetadata: {
-    title: 'Docz Example Typescript',
-    description: 'My awesome app using docz',
+    title: 'The Storygram',
+    description: 'Storygram Documentation',
   },
   plugins: [
     {
@@ -35,7 +35,7 @@ const config = {
       options: {
         themeConfig: {},
         themesDir: 'src',
-        mdxExtensions: ['.md', '.mdx'],
+        mdxExtensions: [ '.md', '.mdx' ],
         docgenConfig: {},
         menu: [],
         mdPlugins: [],
@@ -64,8 +64,8 @@ const config = {
         eb: 'master',
         'edit-branch': 'master',
         config: '',
-        title: 'Docz Example Typescript',
-        description: 'My awesome app using docz',
+        title: 'The Storygram',
+        description: 'Storygram Documentation',
         host: 'localhost',
         port: 3000,
         p: 3000,
